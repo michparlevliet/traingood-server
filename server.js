4732ad2f8d3671595dc5fa3c330a1c3cf975c186
Michelle Parlevliet <michelle0parlevliet@gmail.com>
2023-08-14 19:26:54 -0400

    update cors statement again

--- server.js
+++ server.js
@@ -7,9 +7,7 @@ const cors = require('cors');
 // TEST DB CONNECTION
 const connection = require('./database');
 
-app.use(cors({
-  origin: 'http://localhost:3000/'
-}));
+app.use(cors);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.json());
